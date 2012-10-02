151cfcfdf56d807d7f756bf3090f2a2c1ec4ff61
Tommy <tommy.d.hansen@gmail.com>
2012-10-02 11:56:39 +0200

    FirstPush

---
+++ miniProject/src/com/example/miniproject/MiniProject.java
@@ -0,0 +1,20 @@
+package com.example.miniproject;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MiniProject extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mini_project);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_mini_project, menu);
+        return true;
+    }
+}
